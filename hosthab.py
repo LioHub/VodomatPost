f69542c0d2865936f3ed25e612c689ca7d7118be
LioHub <lioknuckles@gmail.com>
2017-07-27 18:42:43 +0300

    Vodomat

--- hosthab.py
+++ hosthab.py
@@ -11,7 +11,7 @@ config = json.loads(text)
 
 token = config["token"]
 
-bot = telebot.TeleBot(tpken=token)
+bot = telebot.TeleBot(token)
 
 
 from Archive import workbyfile
@@ -59,8 +59,7 @@ def connect(sock, addr):
             if method == "Start":
                 print("Activate:")
                 print(date)
-                bot.send_message(param['idT'],
-                                 "Привет!")
+
                 hostWI = hostbd.get_vodomat(int(param['idv']))
                 print('hostWI:')
                 print(hostWI)
@@ -68,19 +67,15 @@ def connect(sock, addr):
                         print('Hi')
                         try:
                             userbd.update_user(**param)
-                            print('sdfsdf')
                             perTrue = "1"
                             idv = param['idv']
                             hostbd.update_vodomatActivate(idv, perTrue)
-                            print('dasdasdasda')
                             tableSock[int(idv)].update({"locked": True})
                             send(date, idv)
                         except:
                             bot.send_message(param['idT'],
                                              "Приносим вам свои извинения,"
                                              "но водомат временно в не рабочем состоянии!")
-
-
 
             elif method == "Stop":
                 print("Stop:")
@@ -96,56 +91,101 @@ def connect(sock, addr):
                                              "Приносим вам свои извинения,"
                                              "но водомат временно в не рабочем состоянии!")
 
-
-
             elif method == "Answer":
 
                 print("Answer:")
                 print(date)
 
-                print('param:')
-                print(param)
-                idv=param['idv']
+                idv = param['idv']
                 tableSock[int(idv)].update({"locked": False})
-                print('jgfkjb')
-                Status = date['Status']
-                print("Status:")
-                print(Status)
+
+                #статус водоматы с водомата
+                StatusVodomataFromVodomat = date['Status']
+                print("StatusVodomataFromVodomat:")
+                print(StatusVodomataFromVodomat)
+
+                #счет пользоателя с БД
                 #Score of user
                 ScoreOfUser = userbd.get_user(param['idT'])
                 print("ScoreOfUser:2")
                 print(ScoreOfUser)
-
-                SostoyanieVodomata = hostbd.get_vodomat(idv)
-                print("SostoyanieVodomata:3")
-                print(SostoyanieVodomata)
-                TotalPaidBefore = int(SostoyanieVodomata['totalPaid'])
-                print("TotalPaidBefore:4")
-                print(TotalPaidBefore)
-                TotalPaidAfter = Status['totalPaid']
-                print("TotalPaidAfter:5")
-                print(TotalPaidAfter)
                 ScoreOfUser = int(ScoreOfUser['score'])
                 print("ScoreOfUser:6")
                 print(ScoreOfUser)
-                HowMuchWereSpent = TotalPaidAfter - TotalPaidBefore #Сколько потратил
-                HowMuchWere = HowMuchWereSpent + Status['leftFromPaid'] #Сколько было
+
+                #статус водомота с БД
+                StatusVodomataFromDB = hostbd.get_vodomat(idv)
+                print("SostoyanieVodomata:3")
+                print(StatusVodomataFromDB)
+
+                #затраты до
+                TotalPaidBefore = int(StatusVodomataFromDB['totalPaid'])
+                print("TotalPaidBefore:4")
+                print(TotalPaidBefore)
+
+                #затраты после
+                TotalPaidAfter = StatusVodomataFromVodomat['totalPaid']
+                print("TotalPaidAfter:5")
+                print(TotalPaidAfter)
+
+                # Сколько потратил
+                HowMuchWereSpent = TotalPaidAfter - TotalPaidBefore
+                print("HowMuchWereSpent:")
+                print(HowMuchWereSpent)
+
+                # Сколько было
+                HowMuchWere = HowMuchWereSpent + StatusVodomataFromVodomat['leftFromPaid']
+                print("HowMuchWere:")
+                print(HowMuchWere)
+
+                # Сколько он закинул на счет
                 HowMuchWereGiven = HowMuchWere - ScoreOfUser
-                print("Status:7")
-                # score of vodomat
-                ScoreOfVodomat = hostbd.get_vodomat(idv)
-                ScoreOfVodomat = int(ScoreOfVodomat['score'])
-                print("Status:")
-                ScoreOfVodomat = ScoreOfVodomat - HowMuchWereGiven
-                ScoreOfOwner=50
-                ScoreOfOwner = ScoreOfOwner + HowMuchWereSpent
-                print("Status:")
+                print("HowMuchWereGiven:")
+                print(HowMuchWereGiven)
+
+                # Информация водомата с БД
+                InfOfVodomat = hostbd.get_vodomat(idv)
+
+                # Наличка водомата с БД
+                # cashing = int(InfOfVodomat['сashing'])
+                # print("сashing:")
+                # print(cashing)
+                # cashing = cashing + HowMuchWereGiven
+                InfOfVodomat['сashing']= InfOfVodomat['сashing'] + HowMuchWereGiven
+                print("InfOfVodomat['сashing']:")
+                print(InfOfVodomat['сashing'])
+
+
+                # Кредит водомата с БД
+                # credit = int(InfOfVodomat['credit'])
+                print("InfOfVodomat['credit']:")
+                print(InfOfVodomat['credit'])
+
+                credit = HowMuchWereGiven - HowMuchWereSpent
+                if credit > 0:
+                    InfOfVodomat['credit'] = InfOfVodomat['credit'] + credit
+                    print("InfOfVodomat['credit']:")
+                    print(InfOfVodomat['credit'])
+                print("InfOfVodomat['credit']:")
+                print(InfOfVodomat['credit'])
+
+
+                # Продажи водомата с БД
+                # sale = int(InfOfVodomat['sale'])
+                print("InfOfVodomat['sale']:")
+                print(InfOfVodomat['sale'])
+
+                InfOfVodomat['sale'] = InfOfVodomat['sale'] + HowMuchWereSpent
+                print("InfOfVodomat['sale']:")
+                print(InfOfVodomat['sale'])
+
+                # ScoreOfVodomat = ScoreOfVodomat - HowMuchWereGiven
+                # ScoreOfOwner=50
+                # ScoreOfOwner = ScoreOfOwner + HowMuchWereSpent
 
                 bot.send_message(param['idT'], "У вас на счету " + str(param['score']) + "₽")
 
-                print("ScoreOfVodomat:")
-                print(ScoreOfVodomat)
-                hostbd.update_vodomatScore(idv, ScoreOfVodomat)
+                hostbd.update_vodomatScore(idv, InfOfVodomat)
 
                 param['idv'] = 0
                 userbd.update_user(**param)
@@ -153,15 +193,14 @@ def connect(sock, addr):
                 ypar = {'method': 'got', 'param': 'saved'}
                 send(ypar, idv)
 
+
             elif method == "error":
                 bot.send_message(param['idT'],
                                  "В ходе работы произошла ошибка, пожалуйста попробуйте еще разок, ладненько?")
 
-
-
             elif method == "status":  # for get information about hosts
-                prev = hostbd.get_vodomat(param['idv'])
                 idv = param['idv']
+                prev = hostbd.get_vodomat(idv)
                 ypar = {'method': 'got', 'param': 'saved'}
                 if date != prev:
                     hostbd.update_vodomat(**param)
@@ -170,16 +209,16 @@ def connect(sock, addr):
                 send(ypar, idv)
 
             elif method == "connect":
-                hostbd.add_host(param['idv'])
                 idv = param['idv']
+                hostbd.add_host(idv)
                 hostbd.update_vodomat(**param)
                 tableSock.update({date['param']['idv']: {"socket": sock, "locked": False}})
                 workbyfile.write_on_file(date)
                 ypar = {'method': 'got', 'param': 'saved'}
                 send(ypar, idv)
             else:
-                ypar = {"method": "error", "param": {"type": "not method", "args": method}}
                 idv = param['idv']
+                ypar = {"method": "error", "param": {"type": "not method", "args": method}}
                 send(ypar, idv)
 
 
